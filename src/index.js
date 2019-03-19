279504e0f75fb023f3fe8718e9827870476d0828
DzmitryKas <Kasperovich_dima@mail.ru>
2019-03-19 12:37:58 +0300

    fix: fix syntax errors

--- src/index.js
+++ src/index.js
@@ -2,29 +2,33 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {    
+module.exports = function getLoveTrianglesCount(preferences = []) {   
 let spech = [];
-  for (i=1; i<=preferences.length; i++)
+  for (i = 1; i <= preferences.length; i++)
   spech.push(i);
 
 let count = 0;
-  for (j=0; j<preferences.length; j++){  
+  for (j = 0; j < preferences.length; j++){
+    preferences[j];
 
-    for (k=0; k<spech.length; k++) { 
+    for (k=0; k < spech.length; k++) {   
 
-      if (spech[k] == references[j] ) {
+      if (spech[k] == preferences[j] ) {
 
-        for (m=0; m<spech.length; m++){
+        for (m = 0; m < spech.length; m++){
 
           if (preferences[k] == spech[m] && preferences[m] == spech[j] && spech[m] != spech[k] && spech[j] != spech[k]  ) {
-          count = count + 1;          
-          }
-
+          count = count + 1;
+         
         }
+
       }
 
     }
 
   }
-  return (count/3)
+
+}
+
+  return (count / 3);
 };
